3e815603d45f1f0280ac72c08b61a83e726eaaa5
William Jansson <william.jansson@einride.tech>
2023-02-16 15:30:24 +0100

    Add residual analysis plot

--- workfile.r
+++ workfile.r
@@ -11,4 +11,3 @@ print(existing_data)
 # print DEXfat column vs age and do linear regression with a 95% confidence interval
 plot(bodyfatwomen$age, bodyfatwomen$DEXfat, main="DEXfat vs. Age", xlab="Age", ylab="DEXfat", col="red", pch=20, cex=2, bg="white")
 abline(lm(bodyfatwomen$DEXfat ~ bodyfatwomen$age), col="blue", lwd=2)
-
