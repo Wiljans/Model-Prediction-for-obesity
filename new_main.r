1c34fd4558dfce7f48c90e5597841ad36b05d802
tommyarlasjo <79086147+tommyarlasjo@users.noreply.github.com>
2023-02-22 13:40:32 +0100

    new main file added

---
+++ new_main.r
@@ -0,0 +1,27 @@
+
+
+bodyfatwomen = read.csv("bodyfatwomen.csv") #store dataset from .csv
+
+
+# create a matrix of the independent variables
+X = cbind(1, bodyfatwomen$age, bodyfatwomen$weight, bodyfatwomen$height, bodyfatwomen$adiposity, bodyfatwomen$neck, bodyfatwomen$chest, bodyfatwomen$abdomen, bodyfatwomen$hip, bodyfatwomen$thigh, bodyfatwomen$knee, bodyfatwomen$ankle, bodyfatwomen$biceps, bodyfatwomen$forearm, bodyfatwomen$wrist)
+
+# create a matrix of the dependent variable
+y = bodyfatwomen$DEXfat
+
+# compute beta = (X^T*X)^-1*y
+beta = solve(t(X) %*% X) %*% t(X) %*% y
+
+# Compute the residuals and plot them
+residuals = y - X %*% beta
+plot(residuals, main = "Residual Plot for DEXfat", xlab = "Index", ylab = "Residual", col = "red", pch = 20, cex = 1.5, bg = "white")
+
+# plot lines for standard variance
+abline(h = sd(residuals), col = "blue", lwd = 4)
+abline(h = -sd(residuals), col = "blue", lwd = 4)
+
+# include legends for the lines and the points
+legend("topright", legend = c("DEXfat", "Standard Deviation"), col = c("red", "blue"), pch = c(20, 1))
+
+# make points bigger and lines thicker
+par(cex = 1, lwd = 2)
