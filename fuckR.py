84f575a4d1a714564467632533a5dfa2b54f9afd
tommyarlasjo <79086147+tommyarlasjo@users.noreply.github.com>
2023-02-23 20:09:05 +0100

    Test

--- fuckR.py
+++ fuckR.py
@@ -1,16 +1,37 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Import bodyfatwomen.csv
 
 df = pd.read_csv('bodyfatwomen.csv')
 
-# Let DEXfat be the dependent variable and store it in a variable called y
+# Create model
 y = df['DEXfat']
-
-# Let the independent variables be the 10 predictor variables and store them in a variable called X
 X = df[['age', 'waistcirc', 'hipcirc', 'elbowbreadth', 'kneebreadth', 'anthro3a', 'anthro3b', 'anthro3c', 'anthro4']]
 X.insert(0, 'Ones', 1)
 
+# Calculate beta = (X'X)^-1*X'y
+
+beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+y_hat = X.dot(beta)
+
+# calculate residuals
+residuals = y - y_hat
+
+standard_deviation = np.dot(residuals, residuals) / (len(y) - len(X.columns))
+
+print('Standard Variance: ', standard_deviation)
+
+
+# Plot residuals
+plt.figure(figsize=(20, 10))
+plt.scatter(residuals.index, residuals)
+plt.axhline(y=residuals.mean(), color='r', linestyle='-')
+plt.title('Residuals', fontsize = 20)
+plt.xlabel('Observation', fontsize = 15)
+plt.ylabel('Index', fontsize = 15)
+plt.legend(['Error Magnitude', 'Error Mean'])
+plt.show()
 
