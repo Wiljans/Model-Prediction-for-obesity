0a2b66c8910b4efdf34799fc9a4b7879b096bcae
tommyarlasjo <79086147+tommyarlasjo@users.noreply.github.com>
2023-02-23 12:00:39 +0100

    Updated new main

--- fuckR.py
+++ fuckR.py
@@ -5,3 +5,12 @@ import matplotlib.pyplot as plt
 # Import bodyfatwomen.csv
 
 df = pd.read_csv('bodyfatwomen.csv')
+
+# Let DEXfat be the dependent variable and store it in a variable called y
+y = df['DEXfat']
+
+# Let the independent variables be the 10 predictor variables and store them in a variable called X
+X = df[['age', 'waistcirc', 'hipcirc', 'elbowbreadth', 'kneebreadth', 'anthro3a', 'anthro3b', 'anthro3c', 'anthro4']]
+X.insert(0, 'Ones', 1)
+
+
