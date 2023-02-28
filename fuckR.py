54ccbaebd99f9bd4d5d53023bdff2f057cf7797f
tommyarlasjo <79086147+tommyarlasjo@users.noreply.github.com>
2023-02-28 11:10:21 +0100

    Student resiudals added

--- fuckR.py
+++ fuckR.py
@@ -21,20 +21,48 @@ y_hat = X.dot(beta)
 residuals = y - y_hat
 
 standard_deviation = np.sqrt(np.dot(residuals, residuals) / (len(y) - len(X.columns)))
-print('Standard deviation: ', standard_deviation)
+
+
+standardized_residuals = residuals / standard_deviation
 
 # Plot residuals
-plt.figure(figsize=(20, 10))
-plt.scatter(residuals.index, residuals)
-plt.axhline(y=residuals.mean(), color = 'r', linestyle = '-')
+""" plt.figure(figsize=(20, 10))
+plt.scatter(standardized_residuals.index, standardized_residuals)
+plt.axhline(y = standardized_residuals.mean(), color = 'r', linestyle = '-')
 
 # Plot the upper and lower standard deviation lines
-plt.axhline(y=residuals.mean() + standard_deviation, color = 'g', linestyle='--')
-plt.axhline(y=residuals.mean() - standard_deviation, color = 'g', linestyle='--')
+plt.axhline(y=standardized_residuals.mean() + 2, color = 'g', linestyle='--')
+plt.axhline(y=standardized_residuals.mean() - 2, color = 'g', linestyle='--')
 
-plt.title('Residuals', fontsize = 20)
+plt.title('Standardized Residuals', fontsize = 20)
 plt.xlabel('Index', fontsize = 15)
 plt.ylabel('Error Magnitude', fontsize = 15)
-plt.legend(['Error Magnitude', 'Error Mean', 'Standard Deviation'])
+plt.legend(['Error Magnitude', 'Error Mean', 'Two Standard Deviations'])
 plt.show()
+  """
+
+# Calculate the Studentized Residuals
+studentized_residuals = []
+
+for i in range(len(residuals)):
+    studentized_residuals.append(residuals[i] / (standard_deviation * np.sqrt(1 - np.power(X.iloc[i].dot(np.linalg.inv(X.T.dot(X))).dot(X.iloc[i].T), 2))))
+
+#studentized_residuals = residuals / (standard_deviation*np.sqrt(1 - np.power(X.dot(np.linalg.inv(X.T.dot(X))).dot(X.T), 2).diagonal()))
+
+studentized_residuals = pd.Series(studentized_residuals)
+
+
+# Plot the Studentized Residuals
+plt.figure(figsize=(20, 10))
+plt.scatter(studentized_residuals.index, studentized_residuals)
+plt.title('Studentized Residuals', fontsize = 20)
+plt.xlabel('Index', fontsize = 15)
+plt.ylabel('Error Magnitude', fontsize = 15)
+plt.show() 
+
+
+
+
+
+
 
